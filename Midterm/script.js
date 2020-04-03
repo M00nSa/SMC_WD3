7839978fa81aebcb28b43d5b780df39d79918d85
Mahsa <mahsa8mirkhani@gmail..com>
2020-04-02 17:54:18 -0700

    created about page
    added carousel with three photos and some text. added a favicon.

---
+++ Midterm/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //we write everything here
+
+    $('#myCarousel').carousel({
+        interval: 500
+      });
+
+      $('#carouselControl').click(function(){
+        
+        if($('#carouselControl').hasClass('paused')){
+            $('#myCarousel').carousel('cycle');
+            $('#carouselControl').text('pause');
+        }
+        else{
+            $('#myCarousel').carousel('pause');
+            $('#carouselControl').text('play');
+        }
+        $('#carouselControl').toggleClass('paused');
+      });
+    });
