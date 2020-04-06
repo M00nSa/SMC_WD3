9019dc0c898902df4cfa7ace6dfb353a509a6cd6
Mahsa <mahsa8mirkhani@gmail..com>
2020-04-06 01:21:50 -0700

    Added Modal and alert on subscribe button, added a gradientto navbar
    smi-transparent modal and a simple alert added, also gave the transparent navbar a gradient once page is scrolled.

--- Midterm- Blue Novation/script.js
+++ Midterm- Blue Novation/script.js
@@ -1,1 +1,23 @@
-/*making menu to slide */
+
+$(document).ready(function () {
+    $('.toggle').click(function () {
+        $('nav').toggleClass('active')
+    })
+})
+
+$(window).scroll(function(){
+    $('.scrolling-navbar').toggleClass('.scrolled', $(this).scrollTop() > 450);
+    });
+
+$(document).ready(function(){
+    setTimeout(function(){
+$('#exampleModal').modal('show');
+    }, 2000);
+});
+
+$(document).ready(function(){
+    $(".Subscribe").click(function(){
+      alert("Welcome to the unlimited Blue world!");
+    });
+  });
+
