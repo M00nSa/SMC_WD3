8c8017a1f9cf8e6ffad319c5eb75229387732b9f
Mahsa <mahsa8mirkhani@gmail..com>
2020-04-07 16:24:11 -0700

    Tried to make the nav color change when scrolling(edit)

--- Midterm- Blue Novation/script.js
+++ Midterm- Blue Novation/script.js
@@ -1,35 +1,27 @@
 
 $(document).ready(function () {
+
+
     $('.toggle').click(function () {
         $('nav').toggleClass('active')
     })
-})
 
-$(window).scroll(function(){
-    $('.scrolling-navbar').toggleClass('.scrolled', $(this).scrollTop() > 450);
-    });
-
-$(document).ready(function(){
     setTimeout(function(){
-$('#exampleModal').modal('show');
-    }, 2000);
-});
+        $('#exampleModal').modal('show');
+            }, 2000);
 
-$(document).ready(function(){
     $(".Subscribe").click(function(){
-      alert("Welcome to the unlimited Blue world!");
+        alert("Welcome to the unlimited Blue world!");
     });
-  });
 
+    $(".Subscribe").click(function(){
+        alert("Welcome to the unlimited Blue world!");
+      });
 
-
-  //Service carousel and btns:
-
-  $(document).ready(function(){
-    //we write everything here
-
-    $('#myCarousel').carousel({
-        interval: 500
+      $('#myCarousel').carousel({
+        interval: 2000,
+        cycle: true,
+        pause: null
     });
 
     $('#carouselControl').click(function(){
@@ -45,4 +37,42 @@ $(document).ready(function(){
 
         $('#carouselControl').toggleClass('paused');
     });
-});
+
+})
+
+
+
+
+$(window).scroll(function(){
+    var CarouselHeight = $("#myCarousel").height();
+    if($(this).scrollTop() > CarouselHeight){
+        $('.scrolling-navbar').addClass('scrolled');
+    }
+    else{
+        $('.scrolling-navbar').removeClass('scrolled');
+    }
+    });
+
+    $(window).scroll(function(){
+        var CarouselHeight = $("#MyJumbo").height();
+        if($(this).scrollTop() > CarouselHeight){
+            $('.scrolling-navbar').addClass('scrolled');
+        }
+        else{
+            $('.scrolling-navbar').removeClass('scrolled');
+        }
+        });
+
+
+
+       
+
+        $(window).scroll(function(){
+            var CarouselHeight = $("#TeamContainer").height();
+            if($(this).scrollTop() > CarouselHeight){
+                $('.scrolling-navbar').addClass('scrolled');
+            }
+            else{
+                $('.scrolling-navbar').removeClass('scrolled');
+            }
+            });
