00ac4e344b5cd18f14b1f988a72be4fd9ac56f2d
Mahsa <mahsa8mirkhani@gmail..com>
2020-04-17 18:18:46 -0700

    Created footer for all pages, organized <header> <main> <footer>, indication of an active page is with color
    fotter that includes social media, address, phone number, email address and google map. Put <header> <main> <footer> in order and organized what should be in each section. Active page has a different color than other links in the navbar.

--- Midterm- Blue Novation/script.js
+++ Midterm- Blue Novation/script.js
@@ -45,18 +45,17 @@ $(document).ready(function () {
 });
 
 
-function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-    return pattern.test(emailAddress);
-}
 
-$("form.needs-validation").submit(function (event) {
+$("form.needs-validation").submit(function(event){
     event.preventDefault();
 
-    if ($("input.form-control").val() === "") {
-        $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+    if($("input.form-control").val() === "" ){
+      $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
     }
-});
+  });
+
+
+
 
 /*$(window).scroll(function(){
     var CarouselHeight = $("#myCarousel").height();
