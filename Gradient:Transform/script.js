4bf24b8f21bc27a34fc1c6053ddba7a16a6c1262
Mahsa <mahsa8mirkhani@gmail..com>
2020-05-05 12:51:12 -0700

    Created the third page
    added cards with different looks to purchase, added button to add items to cart

--- Gradient:Transform/script.js
+++ Gradient:Transform/script.js
@@ -1,10 +1,36 @@
 
 
-$(document).ready(function() {
 
-    $("#Present").click(function() {
-      $("#Content").show();
-    });
 
-    
+ $(document).ready(function () {
+
+  
+  $("#Present").click(function () {
+      $("#Content").toggle("slow")
+
   });
+
+
+  
+  var size = $("#header2").css("fontSize")
+  $("#header2").hover(
+      function () {
+          $(this).css("fontSize", "2.5rem");
+      },
+      function () {
+          $(this).css("fontSize", size);
+      });
+
+
+      var size = $("#header2-2").css("fontSize")
+  $("#header2-2").hover(
+      function () {
+          $(this).css("fontSize", "2.5rem");
+      },
+      function () {
+          $(this).css("fontSize", size);
+      });
+
+
+
+});
