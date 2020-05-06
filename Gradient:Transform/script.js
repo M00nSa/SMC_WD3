612a49971228cf90acc892136a90e6571f7d4111
Mahsa <mahsa8mirkhani@gmail..com>
2020-05-05 17:03:43 -0700

    Edited Gradient-Transform files based on the feedback

--- Gradient:Transform/script.js
+++ Gradient:Transform/script.js
@@ -33,4 +33,45 @@
 
 
 
+
+      $("form.needs-validation").submit(function (event) {
+        event.preventDefault();
+
+        if ($("input.form-control").val() === "") {
+            $("form.needs-validation").removeClass('needs-validation').addClass(
+                'was-validated');
+        }
+    });
+
+    setTimeout(function () {
+        $('#exampleModal').modal('show');
+    }, 2000);
+
+
+
+    $('.Subscribe').click(function () {
+        $('.alert').show()
+    })
+    
 });
+
+
+
+
+
+$("#Order").click(function () {
+    var productId = $("#productId").val();
+    var productName = $("#productName").val();
+    var productQuantity = $("#productQuantity").val();
+    var data = {
+        'product_id': productId,
+        'product_name': productName,
+        'quantity': productQuantity
+    };
+
+    $.post("/cart/add", data, showDone);
+});
+
+var showDone = function () {
+    /* Make something happen here*/
+}
